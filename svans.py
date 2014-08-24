d65f6b0db2f81892c7a0e0ea2897bf84e6669e9f
Joakim Uddholm <joakim@uddholm.com>
2014-08-24 14:59:54 +0200

    Binary search and first version of stoftsvans.

--- svans.py
+++ svans.py
@@ -19,12 +19,17 @@ def generateData(C, angle, V0, tmax):
 	return z
 
 
-def plot(z):	
+def plot(z, stoftar):	
 	x = [k for k,_,_,_ in z]
 	y = [r for _,r,_,_ in z]
 	
 	fig, ax = plt.subplots()
 	ax.plot(x, y)
+	
+	for stoft in stoftar:
+		x = [k for k,_,_,_ in stoft]
+		y = [r for _,r,_,_ in stoft]
+		ax.plot(x, y, 'r')
 	
 	ax.plot(75, 0, 'x')
 	ax.plot(0, 0, 'yo') # solen   
@@ -43,33 +48,52 @@ def main():
 	
 	def _length(vec):
 		return sqrt(vec[0] ** 2 + vec[1] ** 2)
-	
-	diff = 1
-	while diff > desired_accuracy:
-		g = generateData(C, 2*pi/3, v, 8700) # -0.085			
-		x, y, vx, vy = g[0]
-		i = 1
 		
-		#~ prevdiff = diff = _length((p0[0] - x, p0[1] - y))
-		prevdiff = diff = abs(75 - x)
+	def _curveDistanceToPoint(point, curve):
+		prevdiff = diff = _length((p0[0] - x, p0[1] - y))
 		while diff <= prevdiff and i < len(g):
 			prevdiff = diff			
 			x, y, vx, vy = g[i]	
 			i += 1	
-			diff = abs(75 - x)
-			#~ diff = _length((p0[0] - x, p0[1] - y))				
-		
+			diff = _length((p0[0] - x, p0[1] - y))	
 		i -= 1
-		print g[i]			
-		print p0
 		x, y, vx, vy = g[i]
-		diff = _length((p0[0] - x, p0[1] - y))
-		print diff	
-		v = v - ((y) / (vy))
-		print v
-		break
+		return _length((p0[0] - x, p0[1] - y))
 		
-	plot(g)
+	def _getZeroPoint(curve):
+		x, y, _, _ = curve[0]
+		prev = abs(y)
+		i = 1
+		while y >= 0 and i < len(curve):
+			x, y, _, _ = curve[i]
+			#~ print x,y
+			i += 1
+		i -= 1
+		return curve[i]
+		
+	
+	diff = 1
+	delta = 0.1
+	angle = 2*pi/3
+	while diff > desired_accuracy:
+		curve = generateData(C, angle, v, 3000) # -0.085			
+		
+		#~ _curveDistanceToPoint(p0, g)
+		zero = _getZeroPoint(curve)
+		print zero
+		if zero[0] > p0[0]:
+			v += delta
+		elif zero[0] < p0[0]:
+			v -= delta
+				
+		diff = _length((p0[0] - zero[0], p0[1] - zero[1]))
+		delta /= 2
+		print diff, v
+		#~ break
+		
+	cvals = arange(-1, 1, 0.1)	
+	stoftar = [generateData(c, angle, v, 3000) for c in cvals]
+	plot(curve, stoftar)
 	
 
 if __name__ == '__main__':
